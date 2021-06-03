ae3bf3dee28541150f2c7b07520cb2340d520222
chmodx <chmod777.ke@gmail.com>
2021-06-03 08:58:56 +0300

    feat: update config to read through list of elements

--- src/project_name/settings/base.py
+++ src/project_name/settings/base.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
