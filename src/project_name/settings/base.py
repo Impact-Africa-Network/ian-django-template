d5c4c866b059b73d8ed9f438e6829f9808b1e951
chmodx <chmod777.ke@gmail.com>
2021-10-14 23:05:28 +0300

    chore: add sample AUTH_USER_MODEL setting, incase using ian_account submodule

--- src/project_name/settings/base.py
+++ src/project_name/settings/base.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'rest_framework'
 ]
 
+# AUTH_USER_MODEL = "ian_account.User"
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
